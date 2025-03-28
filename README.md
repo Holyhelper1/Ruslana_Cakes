30d058a0c711e398d9dcdaa210531d927c15fcce
Alexander Shulgin <shulgin.alexandera@gmail.com>
2025-03-28 09:54:34 +0300

    добавлена ссылка с firebase web

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ sass --watch ./src/scss/main.scss ./css/style.css
 
 # Online web
 
-https://holyhelper1.github.io/Ruslana_Cakes/
+https://ruslana-cakes.web.app/
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,69 @@
-// import './firebase';
-// import { Route, Routes } from "react-router";
-// import { Footer } from "./Components/Footer/Footer";
-// import { Header } from "./Components/Header/Header";
-// import { RouteConfig, routes } from "./routes/routes";
-// import { useDispatch } from "react-redux";
-// import { useEffect } from "react";
-// import { initialize } from "./Slices/authSlice";
-
-// const App: React.FC = () => {
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     dispatch(initialize());
-//   }, []);
-//   return (
-//     <>
-//       <Header />
-//       <Routes>
-//         {routes.map(({ path, component: Component }: RouteConfig) => (
-//           <Route key={path} path={path} element={<Component />} />
-//         ))}
-//       </Routes>
-//       <Footer />
-//     </>
-//   );
-// };
-
-// export default App;
-
 import './firebase';
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { Route, Routes } from 'react-router';
+import { Route, Routes } from "react-router";
 import { Footer } from "./Components/Footer/Footer";
 import { Header } from "./Components/Header/Header";
 import { RouteConfig, routes } from "./routes/routes";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 import { initialize } from "./Slices/authSlice";
-import { auth } from './firebase'; // Импортируйте auth из firebase
 
 const App: React.FC = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(initialize());
-    }, [dispatch]);
-
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged((user) => {
-            console.log("User state changed:", user);
-        });
-
-        return () => unsubscribe();
-    }, []);
-
-    return (
-        <>
-            <Header />
-            <Routes>
-                {routes.map(({ path, component: Component }: RouteConfig) => (
-                    <Route key={path} path={path} element={<Component />} />
-                ))}
-            </Routes>
-            <Footer />
-        </>
-    );
+  useEffect(() => {
+    dispatch(initialize());
+  }, []);
+  return (
+    <>
+      <Header />
+      <Routes>
+        {routes.map(({ path, component: Component }: RouteConfig) => (
+          <Route key={path} path={path} element={<Component />} />
+        ))}
+      </Routes>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
+
+// import './firebase';
+// import React, { useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { Route, Routes } from 'react-router';
+// import { Footer } from "./Components/Footer/Footer";
+// import { Header } from "./Components/Header/Header";
+// import { RouteConfig, routes } from "./routes/routes";
+// import { initialize } from "./Slices/authSlice";
+// import { auth } from './firebase'; // Импортируйте auth из firebase
+
+// const App: React.FC = () => {
+//     const dispatch = useDispatch();
+
+//     useEffect(() => {
+//         dispatch(initialize());
+//     }, [dispatch]);
+
+//     useEffect(() => {
+//         const unsubscribe = auth.onAuthStateChanged((user) => {
+//             console.log("User state changed:", user);
+//         });
+
+//         return () => unsubscribe();
+//     }, []);
+
+//     return (
+//         <>
+//             <Header />
+//             <Routes>
+//                 {routes.map(({ path, component: Component }: RouteConfig) => (
+//                     <Route key={path} path={path} element={<Component />} />
+//                 ))}
+//             </Routes>
+//             <Footer />
+//         </>
+//     );
+// };
+
+// export default App;
